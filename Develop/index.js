f49476d9ecd93ef52a602bd9918417905ef51060
Robert Lee <robertlee@Roberts-MacBook-Pro.local>
2023-05-23 17:00:06 -0700

    added the index.js and generatmark

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,70 @@
-// TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project name?.",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "What is this project about?.",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Please choose from the following for the license you used.",
+    choices: ["MIT", "APACHE2.0", "Arthistic-2.0", "afl-3.0", "BSD2", "BSD3", "none"],
+  },
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "What langugaes have you used to create this project?.",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your github username?.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?.",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Please list any contributors by inserting their github username .",
+    default: "",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Was there any test that needed for this project? Please explain if any test were used.",
+  },
+  {
+    type:"input",
+    name:"questions",
+    message:"Any questions for this project?"
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+  }
+];
+
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Please wait for your README file is being created.");
+    writeToFile("./example", generateMarkdown({ ...responses }));
+  });
+}
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,46 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function renderLicenseLink(license){
+    if (license !== "none") {
+        return '\n"[License](#license)\n';
+    }
+    return "";
 }
 
-module.exports = generateMarkdown;
+//Creating a function to generate markdown for README
+function generateMarkdown(data) {
+    return `# ${data.title} 
+
+
+  ${renderLicenseLink(data.license)}
+  ## Title 
+  ${data.title}
+
+
+  ## Description
+  ${data.description}
+
+
+  ## Table of Contents
+  * [Usage](#usage)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  
+  ## How to Use This Application:
+  ${data.usage}
+  
+  ## Contributors
+  ${data.contributors}
+
+  ## Testing
+  ${data.test}
+
+  ## Questions
+  ${data.questions}
+
+  ## Contact Info
+  *Github username:${data.github}
+  *Email: ${data.email}
+  `;
+  }
+  
+  module.exports = generateMarkdown;
